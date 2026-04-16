e6cdaeef5185973ec911b12b0d90721f23d2445b
Edward Ji <jiziao6@gmail.com>
2026-04-16 22:17:52 +1000

    Remove unused language env integrations

--- zsh/.config/zsh/zshrc.d/24-prompt.zsh
+++ zsh/.config/zsh/zshrc.d/24-prompt.zsh
@@ -1,28 +1,10 @@
 # Prompt end of line mark
 PROMPT_EOL_MARK='%F{8}󰌑%f'
 
-##########
-# Prompt #
-##########
 setopt PROMPT_SUBST
 
 # Working directory
 PROMPT='%B%3~%b '
-
-# Python venv integration
-PROMPT+='${VIRTUAL_ENV_PROMPT:+󰌠 ${VIRTUAL_ENV_PROMPT% } }'
-
-# Jenv integration
-precmd_jenv_info() {
-    if (( $+commands[jenv] )); then
-        jenv_info="$(jenv version-name)"
-        if [[ "${jenv_info}" = system ]]; then
-            jenv_info=
-        fi
-    fi
-}
-precmd_functions+=( precmd_jenv_info )
-PROMPT+='${jenv_info:+"󰬷 $jenv_info "}'
 
 # Git integration
 zstyle ':vcs_info:git*' formats '%b'
@@ -39,10 +21,6 @@ PROMPT+='${vcs_info_msg_0_:+" $vcs_info_msg_0_ "}'
 
 # Exit code and superuser integration
 PROMPT+='%(?.%F{green}.%F{red})%(!..)%f '
-
-################
-# Right Prompt #
-################
 
 # show hostname
 RPROMPT='%F{8}%n@%m%f'

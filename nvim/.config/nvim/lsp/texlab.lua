8ea68fe7c8027f6e01fbd6a0a868fd84261a3e72
Edward Ji <jiziao6@gmail.com>
2026-05-07 00:13:21 +1000

    Remove Skim app

--- nvim/.config/nvim/lsp/texlab.lua
+++ nvim/.config/nvim/lsp/texlab.lua
@@ -1,12 +1,7 @@
 return {
     settings = {
         texlab = {
-            forwardSearch = {
-                executable = '/Applications/Skim.app/Contents/SharedSupport/displayline',
-                args = { '-n', '-g', '%l', '%p', '%f' },
-            },
             build = {
-                forwardSearchAfter = true,
                 onSave = true,
             },
         },
